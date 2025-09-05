dc3552e345f01a8be2f1f0daf0261675c0a99768
pankaj-gkm <155955669+pankaj-gkm@users.noreply.github.com>
2025-09-05 12:04:42 +0530

    remove seperate config file

---
+++ config/index.js
@@ -0,0 +1,18 @@
+const HT_CONFIG = {
+  currency: {
+    name: "GO Coin",
+    logo: "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA0L3YxMTYxLWItMDQ0LnBuZw.png",
+  },
+};
+
+const INDIAN_EXPRESS = {
+  currency: {
+    name: "Minecraft Coin",
+    logo: "https://p1.hiclipart.com/preview/896/990/447/minecraft-hd-icon-mac-pc-minecraft-icon-512-png-icon.jpg",
+  },
+};
+
+const LOYALTY_PROTOCOL_CONFIG = {
+  "ht-kstore-india": HT_CONFIG,
+  "indian-express": INDIAN_EXPRESS,
+};
