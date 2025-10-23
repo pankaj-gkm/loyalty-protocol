0dae491fdd151d2bb8b8415abea72370b420e6c6
pankaj-gkm <155955669+pankaj-gkm@users.noreply.github.com>
2025-10-23 11:10:08 +0530

    feat: theme

--- config/ht-kstore-india/index.js
+++ config/ht-kstore-india/index.js
@@ -1,7 +1,17 @@
 export const HT_CONFIG = {
   currency: {
-    name: "GO Coin",
-    logo: "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA0L3YxMTYxLWItMDQ0LnBuZw.png",
+    "K-Cash": {
+      name: "Coins",
+      logo: "https://images.hindustantimes.com/sso/prod/loyalty-reward-sdk/images/goCoinHT.svg",
+    },
+    "K-Points": {
+      name: "T-Points",
+      logo: "https://s2.coinmarketcap.com/static/img/coins/128x128/1958.png",
+    },
+    rKGeN: {
+      name: "T Coin",
+      logo: "https://s2.coinmarketcap.com/static/img/coins/128x128/32196.png",
+    },
   },
   showHeader: false,
 };
